d250f4cbb97d88604fef58c78a7524d481be484a
kimjm9911 <kimjm9911@naver.com>
2023-11-22 12:21:00 +0900

    App.js 수정

--- front-end/kcalculator/src/App.js
+++ front-end/kcalculator/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider  } from 'styled-components/native';
-import Navigation from './navigations';
+//import Navigation from './navigations';
 import { theme } from './theme';
 import { ProgressProvider, UserProvider } from './contexts';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigation from './navigations/ProfileStack';
 
 const App = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <UserProvider>
-                <ProgressProvider>
-                    <StatusBar barStyle="dark-content" />
-                    <Navigation />
-                </ProgressProvider>
-            </UserProvider>
-        </ThemeProvider>
+        <NavigationContainer>
+            <StackNavigation />
+        </NavigationContainer>
     );
 };
 
--- front-end/kcalculator/src/navigations/index.js
+++ front-end/kcalculator/src/navigations/index.js
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
-import { Spinner } from '../components';
-import { ProgressContext } from '../contexts';
-import MainStack from './MainStack';
+//import { Spinner } from '../components';
+//import { ProgressContext } from '../contexts';
+//import MainStack from './MainStack';
 
 const Navigation = () => {
     const { inProgress } = useContext(ProgressContext);
 
     return (
         <NavigationContainer>
-            <MainStack />
-            {inProgress && <Spinner />}
+            <AuthStack />
         </NavigationContainer>
     );
 };
