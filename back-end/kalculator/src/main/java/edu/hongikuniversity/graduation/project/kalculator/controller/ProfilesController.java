85637c3d5d5cdbd9fc6bf7a09dcee709c70157fd
jmd5314 <jmd5314@gmail.com>
2023-11-27 15:40:19 +0900

    Update ProfilesController.java

--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/controller/ProfilesController.java
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/controller/ProfilesController.java
@@ -5,6 +5,7 @@ import edu.hongikuniversity.graduation.project.kalculator.domain.dto.DietModeReq
 import edu.hongikuniversity.graduation.project.kalculator.domain.dto.ProfilesResponseDto;
 import edu.hongikuniversity.graduation.project.kalculator.domain.dto.ProfilesSaveRequestDto;
 import edu.hongikuniversity.graduation.project.kalculator.service.ProfilesService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.engine.transaction.jta.platform.internal.AtomikosJtaPlatform;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
@@ -24,13 +25,17 @@ public class ProfilesController {
         return profilesService.save(requestDto);
     }
     @PostMapping("/saveDietMode")
-    public ResponseEntity<Long> saveDietMode(@RequestBody DietModeRequestDto requestDto) {
+    public Long saveDietMode(@RequestBody DietModeRequestDto requestDto) {
         DietMode dietMode = DietMode.valueOf(requestDto.getDietMode().toUpperCase());
         Profiles profiles = profilesService.findById(requestDto.getProfileId());
         profiles.updateProfiles(profiles.getTargetWeight(),profiles.getAge(),profiles.getGender(),profiles.getHeight()
         ,profiles.getWeight(),profiles.getActivityLevel(),profiles.getPurposeOfUse(),dietMode);
         profilesService.save(profiles);
-        return ResponseEntity.ok(profiles.getProfileId());
+        return profiles.getProfileId();
+    }
+    @GetMapping("/targetCalrories")
+    public Long calculateCalrories(){
+        
     }
 }
 
