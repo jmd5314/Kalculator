afa3bd0cf6c71fffa0970bfb69ee88b178538fef
jmd5314 <jmd5314@gmail.com>
2024-02-06 13:55:09 +0900

    좋아요 레포지토리 구현

---
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/repository/HeartsRepository.java
@@ -0,0 +1,12 @@
+package edu.hongikuniversity.graduation.project.kalculator.repository;
+
+import edu.hongikuniversity.graduation.project.kalculator.domain.Hearts;
+import edu.hongikuniversity.graduation.project.kalculator.domain.Posts;
+import edu.hongikuniversity.graduation.project.kalculator.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HeartsRepository extends JpaRepository<Hearts,Long> {
+    Optional<Hearts> findByUsersAndPosts(Users users, Posts posts);
+}
