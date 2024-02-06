e85c9d730dc730dc0d8b505dad99beb32951b532
jmd5314 <jmd5314@gmail.com>
2024-02-06 16:05:55 +0900

    PostResponseDto 에 postId 추가

--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/dto/PostsResponseDto.java
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/dto/PostsResponseDto.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import java.time.LocalDate;
 @Getter
 public class PostsResponseDto {
+    private Long postId;
     private String userId;
     private String title;
     private String content;
     private LocalDate creationDate;
     public PostsResponseDto(Posts entity){
+        this.postId = entity.getPostId();
         this.userId = entity.getUsers().getUserId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
