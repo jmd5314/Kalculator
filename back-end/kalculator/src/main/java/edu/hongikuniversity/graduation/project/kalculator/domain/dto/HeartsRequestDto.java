4443f499aedeef5ff6914cdebc6ee89935c4ec23
jmd5314 <jmd5314@gmail.com>
2024-04-03 05:50:42 +0900

    [Fix] 좋아요 관련 Dto 삭제 및 수정

--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/dto/HeartsRequestDto.java
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/dto/HeartsRequestDto.java
@@ -1,13 +1,8 @@
 package edu.hongikuniversity.graduation.project.kalculator.domain.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class HeartsRequestDto {
     private Long postId;
-    public HeartsRequestDto(String userId,Long postId){
-        this.postId = postId;
-    }
 }
--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/dto/PostsConfirmRequestDto.java
+++
@@ -1,9 +0,0 @@
-package edu.hongikuniversity.graduation.project.kalculator.domain.dto;
-
-import lombok.Getter;
-
-@Getter
-public class PostsConfirmRequestDto {
-    private Long postId;
-    private String userId;
-}
