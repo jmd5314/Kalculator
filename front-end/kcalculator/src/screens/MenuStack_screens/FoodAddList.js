32da7bef2ae96ac3a53682b1bc9c877204bba3e2
jmd5314 <jmd5314@gmail.com>
2024-05-07 12:54:03 +0900

    [Fix] 직렬화 에러 수정
    음식 기록을 처음 할때마다
    Non-serializable values were found in the navigation state 에러가 발생했는데,  함수를 navigation parameters로 전달하려고 하여 React Navigation에 직렬화할 수 없는 값을 전달했기 때문에 발생
    Local Storage 사용하는 방식으로 변경

--- front-end/kcalculator/src/screens/MenuStack_screens/FoodAddList.js
+++ front-end/kcalculator/src/screens/MenuStack_screens/FoodAddList.js
@@ -1,147 +1,146 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialIcons } from '@expo/vector-icons';
 import config from "../config";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const backendUrl = config.backendUrl;
-const FoodAddList = ({ navigation, route }) => {
-  const [selectedItemList, setSelectedItemList] = useState(route.params.selectedItemList || []);
-    // 이 부분에서 함수를 직렬화 가능한 형태로 변경
-    const updateSelectedItemList = useCallback((updatedItemList) => {
-        setSelectedItemList(updatedItemList);
-        // MenuSearch로 업데이트된 항목 전달
-        route.params.updateSelectedItemList(updatedItemList);
-    }, [route.params.updateSelectedItemList]);
 
-    const totalCalories = selectedItemList ? selectedItemList.reduce((acc, item) => acc + parseInt(item.calories, 10)* item.quantity, 0) : 0;
-    const totalCarbs = selectedItemList ? selectedItemList.reduce((acc, item) => acc + parseInt(item.carbs, 10)* item.quantity, 0) : 0;
-    const totalProtein = selectedItemList ? selectedItemList.reduce((acc, item) => acc + parseInt(item.protein, 10)* item.quantity, 0) : 0;
-    const totalFat = selectedItemList ? selectedItemList.reduce((acc, item) => acc + parseInt(item.fat, 10)* item.quantity, 0) : 0;
+const FoodAddList = ({ navigation }) => {
+    const [selectedItemList, setSelectedItemList] = useState([]);
 
-    const handleDeleteItem = (itemId) => {
-      // 선택한 음식 항목에서 itemId에 해당하는 항목을 제거
-      const updatedItemList = selectedItemList.filter(item => item.id !== itemId);
-  
-      // 상태 업데이트
-      setSelectedItemList(updatedItemList);
-  
-      // MenuSearch로 업데이트된 항목 전달
-      route.params.updateSelectedItemList(updatedItemList);
+    useEffect(() => {
+        const loadItemList = async () => {
+            const itemList = await AsyncStorage.getItem('selectedItemList');
+            if (itemList !== null) {
+                setSelectedItemList(JSON.parse(itemList));
+            }
+        };
+
+        loadItemList();
+    }, []);
+
+    const calculateTotals = () => {
+        const totals = selectedItemList.reduce((acc, item) => {
+            acc.calories += item.calories * item.quantity;
+            acc.carbs += item.carbs * item.quantity;
+            acc.protein += item.protein * item.quantity;
+            acc.fat += item.fat * item.quantity;
+            return acc;
+        }, { calories: 0, carbs: 0, protein: 0, fat: 0 });
+        return totals;
     };
 
-  const sendFoodToServer = async () => {
-    const token = await AsyncStorage.getItem('token');
-    if (!token) {
-        console.error('Token not found');
-        return;
-    }
+    const handleDeleteItem = (itemId) => {
+        const updatedItemList = selectedItemList.filter(item => item.id !== itemId);
+        setSelectedItemList(updatedItemList);
+        AsyncStorage.setItem('selectedItemList', JSON.stringify(updatedItemList));
+    };
 
-    const foodDataList = selectedItemList.map(item => ({
-        foodName: item.title, 
-        calories: item.calories,
-        fats: item.fat,
-        carbohydrates: item.carbs,
-        proteins: item.protein,
-        quantity: item.quantity,
-        mealType:item.mealType
-    }));
+    const sendFoodToServer = async () => {
+        const token = await AsyncStorage.getItem('token');
+        if (!token) {
+            console.error('토큰을 찾을 수 없습니다.');
+            return;
+        }
 
-    // 서버에 네트워크 요청을 보냅니다
-    fetch(`${backendUrl}/api/foodRecords/save`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify(foodDataList),
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('네트워크 응답이 정상이 아닙니다');
-            }
-            return response.json();
+        const foodDataList = selectedItemList.map(item => ({
+            foodName: item.title,
+            calories: item.calories,
+            fats: item.fat,
+            carbohydrates: item.carbs,
+            proteins: item.protein,
+            quantity: item.quantity,
+            mealType: item.mealType
+        }));
+
+        fetch(`${backendUrl}/api/foodRecords/save`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify(foodDataList),
         })
-        .then(data => {
-            console.log('음식정보가 성공적으로 전송되었습니다:', data);
-            navigation.navigate("Menu",{ refreshKey: Math.random().toString() });
-        })
-        .catch(error => {
-            console.error('음식정보 전송 중 오류 발생:', error);
-        });
-  };
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('네트워크 응답이 정상이 아닙니다');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('음식 정보가 성공적으로 전송되었습니다:', data);
+                // 음식 정보 전송 후 로컬 저장소에서 항목 목록을 비웁니다.
+                AsyncStorage.setItem('selectedItemList', JSON.stringify([]));
+                setSelectedItemList([]);
+                navigation.navigate("Menu");
+            })
+            .catch(error => {
+                console.error('음식 정보 전송 중 오류 발생:', error);
+            });
+    };
 
-  return (
-    <SafeAreaView style={{ backgroundColor: 'white' }}>
-      <View>
-        <Text style={{ fontSize: 30, marginLeft: 15, marginBottom: 15 }}>추가한 음식</Text>
-      </View>
-      <View>
-        <Text style={{ fontSize: 15, marginLeft: 15, marginBottom: 5 }}>
-            추가한 음식들의 총 칼로리 및 영양정보
-        </Text>
-      </View>
-      <View style={styles.summaryContainer}>
-      {console.log(selectedItemList)}
-        <Text style={styles.boxText}>총 칼로리: {totalCalories} kcal</Text>
-        <Text style={styles.boxText}>총 탄수화물: {totalCarbs} g</Text>
-        <Text style={styles.boxText}>총 단백질: {totalProtein} g</Text>
-        <Text style={styles.boxText}>총 지방: {totalFat} g</Text>
-      </View>
-      <View style={styles.itemListContainer}>
-        <Text style={{ fontSize: 15, marginLeft: 15, marginBottom: 5 }}>추가한 음식 항목</Text>
-        <FlatList
-            data={selectedItemList}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-                <View style={styles.itemBox}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10 }}>
-                        <Text style={{ fontSize: 15 }}>{item.title}</Text>
-                        <TouchableOpacity onPress={() => handleDeleteItem(item.id)}>
-                            <MaterialIcons name="remove" size={24} color="red" />
-                        </TouchableOpacity>
+    const { calories, carbs, protein, fat } = calculateTotals();
+
+    return (
+        <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
+            <Text style={{ fontSize: 30, marginLeft: 15, marginBottom: 15 }}>추가한 음식</Text>
+            <Text style={{ fontSize: 15, marginLeft: 15, marginBottom: 5 }}>추가한 음식들의 총 칼로리 및 영양정보</Text>
+            <View style={styles.summaryContainer}>
+                <Text style={styles.boxText}>총 칼로리: {calories} kcal</Text>
+                <Text style={styles.boxText}>총 탄수화물: {carbs} g</Text>
+                <Text style={styles.boxText}>총 단백질: {protein} g</Text>
+                <Text style={styles.boxText}>총 지방: {fat} g</Text>
+            </View>
+            <FlatList
+                data={selectedItemList}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.itemBox}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10 }}>
+                            <Text style={{ fontSize: 15 }}>{item.title}</Text>
+                            <TouchableOpacity onPress={() => handleDeleteItem(item.id)}>
+                                <MaterialIcons name="remove" size={24} color="red" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
-            )}
-        />
-      </View>
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <TouchableOpacity style={{ 
-            height: 50, width: 350, backgroundColor: '#39D02C', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}
-            onPress={() => {
-              sendFoodToServer();
-            }}
-            >
-            <Text style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>기록완료</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+                )}
+            />
+            <TouchableOpacity style={styles.button}
+                              onPress={sendFoodToServer}>
+                <Text style={{ color: 'white', fontSize: 20 }}>기록완료</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  summaryContainer: {
-    marginTop: 5,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 10,
-  },
-  itemListContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  itemBox: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    padding: 5,
-    marginVertical: 5,
-  },
-  boxText: {
-    fontSize: 18, 
-    marginLeft: 15, 
-    marginBottom: 5,
-  },
+    summaryContainer: {
+        padding: 10,
+        backgroundColor: '#f0f0f0',
+        borderRadius: 10,
+        margin: 15
+    },
+    itemBox: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        padding: 5,
+        marginVertical: 5,
+        marginHorizontal: 15
+    },
+    boxText: {
+        fontSize: 18,
+        marginBottom: 5
+    },
+    button: {
+        height: 50,
+        backgroundColor: '#39D02C',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        margin: 15
+    }
 });
 
 export default FoodAddList;
--- front-end/kcalculator/src/screens/MenuStack_screens/MenuSearch.js
+++ front-end/kcalculator/src/screens/MenuStack_screens/MenuSearch.js
@@ -4,54 +4,42 @@ import axios from 'axios';
 import { MaterialIcons } from '@expo/vector-icons';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
 const MenuSearch = ({ navigation }) => {
-    const [ searchText, setSearchText ] = useState('');
-    const [ searchResults, setSearchResults ] = useState([]);
-    const [ selectedItem, setSelectedItem ] = useState(null);
-    const [ selectedItemList, setSelectedItemList ] = useState([]);
-    const [ selectedItemQuantity, setSelectedItemQuantity ] = useState(0);
+    const [searchText, setSearchText] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [selectedItemList, setSelectedItemList] = useState([]);
+    const [selectedItemQuantity, setSelectedItemQuantity] = useState(0);
 
     const updateSelectedItemList = useCallback((updatedList) => {
         setSelectedItemList(updatedList);
     }, []);
 
-    // useEffect를 사용하여 화면 포커스 시 이벤트 추가
     useEffect(() => {
-        // MenuSearch 화면이 포커스를 얻을 때마다 동기화
-        const unsubscribe = navigation.addListener('focus', () => {
+        const unsubscribe = navigation.addListener('focus', async () => {
             console.log('MenuSearch 화면이 포커스를 얻었습니다.');
-            // 여기서 서버에서 데이터를 가져오거나 다른 동기화 작업을 수행할 수 있습니다.
-            // 예시: setSelectedItemList(새로운 데이터);
+            const storedItems = await AsyncStorage.getItem('selectedItemList');
+            if (storedItems) {
+                setSelectedItemList(JSON.parse(storedItems));
+            }
         });
 
-        return () => {
-            unsubscribe();
-        };
+        return unsubscribe;
     }, [navigation]);
+
     const handleButton = async () => {
         try {
-            // AsyncStorage에서 mealType 가져오기
             const mealType = await AsyncStorage.getItem('selectedMealType');
-
-            // 선택된 음식 목록을 준비
             const selectedItemListWithMealType = selectedItemList.map(item => ({
                 ...item,
-                mealType: mealType || 'defaultMealType', // 기본값은 'defaultMealType'
+                mealType: mealType || 'defaultMealType',
             }));
-
-            // FoodAddList 화면으로 이동하면서 selectedItemList 및 updateSelectedItemList 함수 전달
-            navigation.navigate('FoodAddList', {
-                selectedItemList: selectedItemListWithMealType,
-                updateSelectedItemList,
-            });
+            await AsyncStorage.setItem('selectedItemList', JSON.stringify(selectedItemListWithMealType));
+            navigation.navigate('FoodAddList');
         } catch (error) {
             console.error('AsyncStorage에서 mealType을 가져오는 중 오류 발생:', error);
         }
     };
-
-
 
     const handleSearch = async () => {
         try {
@@ -59,22 +47,17 @@ const MenuSearch = ({ navigation }) => {
             const response = await axios.get(
                 `http://openapi.foodsafetykorea.go.kr/api/a9ff4312e52040419e84/I2790/json/1/20/DESC_KOR="${searchText}"`
             );
-
             const resultData = response.data?.I2790?.row || [];
-
-            // Extract relevant information for each item in the resultData array
             const formattedResults = resultData.map(item => ({
                 id: item.FOOD_CD,
                 title: item.DESC_KOR,
                 calories: item.NUTR_CONT1,
-                carbs: item.NUTR_CONT2||0,
-                protein: item.NUTR_CONT3||0,
-                fat: item.NUTR_CONT4||0,
+                carbs: item.NUTR_CONT2 || 0,
+                protein: item.NUTR_CONT3 || 0,
+                fat: item.NUTR_CONT4 || 0,
                 servingSize: item.SERVING_SIZE,
                 unit: item.SERVING_UNIT,
             }));
-
-            // Update the searchResults state with the formatted results
             setSearchResults(formattedResults);
         } catch (error) {
             console.error('검색 중 오류 발생:', error.message);
@@ -82,64 +65,39 @@ const MenuSearch = ({ navigation }) => {
     };
 
     const handleItemSelected = (item) => {
-        // Set the selected item and show details
         setSelectedItem(item);
     };
 
     const handleInputCertainItemToArray = () => {
-        try {
-            if (selectedItem) {
-                if (selectedItemQuantity > 0) {
-                    const selectedItemWithQuantity = {
-                        ...selectedItem,
-                        quantity: selectedItemQuantity,
-                    };
-
-                    setSelectedItemList([...selectedItemList, selectedItemWithQuantity]);
-                    console.log(`선택한 음식이 추가되었습니다. 수량은 ${selectedItemWithQuantity.quantity}개 입니다.`);
-                    console.log(selectedItemList);
-
-                    setSelectedItemQuantity(0);
-                    setSelectedItem(null);
-                } else {
-                    console.log('음식이 선택되지 않았습니다.');
-                }
-            } else {
-                console.log('선택한 음식이 없습니다.');
-                setSelectedItem(null);
-            }
-        } catch (error) {
-            console.error('에러가 발생하였습니다.', error);
+        if (selectedItem && selectedItemQuantity > 0) {
+            const selectedItemWithQuantity = {
+                ...selectedItem,
+                quantity: selectedItemQuantity,
+            };
+            setSelectedItemList([...selectedItemList, selectedItemWithQuantity]);
+            console.log(`선택한 음식이 추가되었습니다. 수량은 ${selectedItemWithQuantity.quantity}개 입니다.`);
+            setSelectedItemQuantity(0);
+            setSelectedItem(null);
+        } else {
+            console.log('음식을 선택해주세요.');
         }
     };
 
-
-
-        const [isButtonPressed, setButtonPressed] = useState(false);
-
-        const handleButtonPress = () => {
-            // 버튼이 눌렸을 때 실행할 로직 추가
-
-            setButtonPressed(true);
-        };
-
     return (
         <View style={styles.container}>
-            <View style={{ marginBottom: 20, marginLeft: 10,marginTop:20, flexDirection: 'row' }}>
-                <Text style={{ fontSize: 25, marginRight: 180,fontWeight:"bold" }}>음식 검색</Text>
-            <TouchableOpacity
-                style={[
-                    styles.buttonContainer,
-                    isButtonPressed ? styles.buttonPressed : null,
-                ]}
-                onPress= {handleButton}
-            >
-                <Text style={styles.buttonText}>{`${selectedItemList.length}`}</Text>
-            </TouchableOpacity>
-
+            <View style={{ marginBottom: 20, marginLeft: 10, marginTop: 20, flexDirection: 'row' }}>
+                <Text style={{ fontSize: 25, marginRight: 180, fontWeight: "bold" }}>음식 검색</Text>
+                <TouchableOpacity
+                    style={[
+                        styles.buttonContainer,
+                        { backgroundColor: selectedItemList.length > 0 ? '#2ecc71' : '#e74c3c' },
+                    ]}
+                    onPress={handleButton}
+                >
+                    <Text style={styles.buttonText}>{`${selectedItemList.length}`}</Text>
+                </TouchableOpacity>
             </View>
-
-            <View style={{ flexDirection: 'row', marginBottom: 10,marginLeft:10 }}>
+            <View style={{ flexDirection: 'row', marginBottom: 10, marginLeft: 10 }}>
                 <TextInput
                     style={styles.input}
                     placeholder="검색어를 입력하세요"
@@ -156,50 +114,42 @@ const MenuSearch = ({ navigation }) => {
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <TouchableOpacity onPress={() => handleItemSelected(item)}>
-                        <View style={[styles.resultItem, {marginLeft:10}]}>
+                        <View style={[styles.resultItem, { marginLeft: 10 }]}>
                             <Text>{item.title}</Text>
                         </View>
                     </TouchableOpacity>
                 )}
             />
-
-            {/* Display item details */}
             {selectedItem && (
                 <View style={styles.itemDetailsContainer}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: '', marginTop: 10 }}>
-                    <Text style= {{marginRight: 100}}>칼로리: {selectedItem.calories} kcal</Text>
-
-                                <TouchableOpacity onPress={() => setSelectedItemQuantity(selectedItemQuantity > 0 ? selectedItemQuantity - 1 : 0)}>
-                                    <Text style={{marginHorizontal: 5}}>-</Text>
-                                </TouchableOpacity>
-                                <Text style={{marginHorizontal: 5}}>{selectedItemQuantity}</Text>
-                                <TouchableOpacity onPress={() => setSelectedItemQuantity(selectedItemQuantity + 1)}>
-                                    <Text style={{marginHorizontal: 5}}>+</Text>
-                                </TouchableOpacity>
-
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: 10 }}>
+                        <Text style={{ marginRight: 100 }}>칼로리: {selectedItem.calories} kcal</Text>
+                        <TouchableOpacity onPress={() => setSelectedItemQuantity(Math.max(0, selectedItemQuantity - 1))}>
+                            <Text style={{ marginHorizontal: 5 }}>-</Text>
+                        </TouchableOpacity>
+                        <Text style={{ marginHorizontal: 5 }}>{selectedItemQuantity}</Text>
+                        <TouchableOpacity onPress={() => setSelectedItemQuantity(selectedItemQuantity + 1)}>
+                            <Text style={{ marginHorizontal: 5 }}>+</Text>
+                        </TouchableOpacity>
                     </View>
-
                     <Text>탄수화물: {selectedItem.carbs} g</Text>
                     <Text>지방: {selectedItem.fat} g</Text>
                     <Text>단백질: {selectedItem.protein} g</Text>
                     <Text>1회제공량: {selectedItem.servingSize} g</Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: '', marginTop: 10 }}>
-                     <TouchableOpacity onPress={() => setSelectedItem(null)}>
-                        <Text style={{ marginTop: 10 }}>닫기</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => {
-                            handleButtonPress();
-                            handleInputCertainItemToArray();
-                        }}
-                    >
-                        <Text style={{ marginTop: 10, marginLeft: 200 }}>추가하기</Text>
-                     </TouchableOpacity>
-                     </View>
-
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: 10 }}>
+                        <TouchableOpacity onPress={() => setSelectedItem(null)}>
+                            <Text style={{ marginTop: 10 }}>닫기</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                handleInputCertainItemToArray();
+                            }}
+                        >
+                            <Text style={{ marginTop: 10, marginLeft: 200 }}>추가하기</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             )}
-
         </View>
     );
 };
@@ -212,21 +162,21 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 50,
-        width: '85%', // 전체 너비의 85%를 사용
+        width: '85%',
         borderColor: '#CCCCCC',
         borderWidth: 1,
         paddingHorizontal: 10,
-        borderRadius: 25, // 입력창에 둥근 모서리 적용
-        fontSize: 16, // 더 큰 글꼴 크기 사용
-        color: '#333', // 입력 글꼴 색상 조정
+        borderRadius: 25,
+        fontSize: 16,
+        color: '#333',
     },
     resultItem: {
         paddingVertical: 10,
         paddingHorizontal: 10,
         borderBottomWidth: 1,
         borderBottomColor: '#CCCCCC',
-        backgroundColor: '#FFFFFF', // 각 아이템 배경색 추가
-        marginTop: 2, // 아이템 간 간격 추가
+        backgroundColor: '#FFFFFF',
+        marginTop: 2,
     },
     itemDetailsContainer: {
         padding: 20,
@@ -234,8 +184,8 @@ const styles = StyleSheet.create({
         marginTop: 20,
         borderWidth: 1,
         borderColor: '#CCCCCC',
-        borderRadius: 12, // 상세 정보 패널에 둥근 모서리 적용
-        shadowOpacity: 0.1, // 그림자 효과 추가
+        borderRadius: 12,
+        shadowOpacity: 0.1,
         shadowRadius: 5,
         shadowColor: '#000000',
         shadowOffset: { height: 2, width: 2 },
@@ -243,20 +193,15 @@ const styles = StyleSheet.create({
     buttonContainer: {
         width: 50,
         height: 50,
-        backgroundColor: '#2ecc71',
         borderRadius: 25,
         alignItems: 'center',
         justifyContent: 'center',
-        marginLeft: 10, // 버튼 위치 조정
-    },
-    buttonPressed: {
-        backgroundColor: '#e74c3c',
+        marginLeft: 10,
     },
     buttonText: {
         color: '#ffffff',
         fontWeight: 'bold',
     },
 });
-
 
 export default MenuSearch;
