ad58ae612a9e5d35823d64a97d21c71e8d0eec33
jmd5314 <jmd5314@gmail.com>
2024-05-09 13:55:18 +0900

    [Feat] 그룹 상태 변경 스케줄러 추가

--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/service/BattleGroupsService.java
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/service/BattleGroupsService.java
@@ -1,6 +1,7 @@
 package edu.hongikuniversity.graduation.project.kalculator.service;
 
 import edu.hongikuniversity.graduation.project.kalculator.domain.BattleGroups;
+import edu.hongikuniversity.graduation.project.kalculator.domain.BattleStatus;
 import edu.hongikuniversity.graduation.project.kalculator.domain.GroupMembership;
 import edu.hongikuniversity.graduation.project.kalculator.repository.BattleGroupsRepository;
 import edu.hongikuniversity.graduation.project.kalculator.repository.GroupMembershipRepository;
@@ -8,6 +9,9 @@ import edu.hongikuniversity.graduation.project.kalculator.repository.UsersReposi
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +23,27 @@ public class BattleGroupsService {
     public Long save (BattleGroups battleGroups){
         return battleGroupsRepository.save(battleGroups).getGroupId();
     }
-    public void joinGroup(Long groupId,Long userId){
-        GroupMembership membership = new GroupMembership();
+    @Transactional
+    public void updateGroupStatuses() {
+        List<BattleGroups> allGroups = battleGroupsRepository.findAll();
+        LocalDate today = LocalDate.now();
+        for (BattleGroups group : allGroups) {
+            if (group.getStatus() == BattleStatus.RECRUITING && !group.getStartDate().isAfter(today)) {
+                group.setStatus(BattleStatus.PROGRESS);
+            } else if (group.getStatus() == BattleStatus.PROGRESS && !group.getEndDate().isAfter(today)) {
+                group.setStatus(BattleStatus.COMPLETED);
+            }
+        }
+        battleGroupsRepository.saveAll(allGroups);
+    }
+    public List<BattleGroups> findRecruitingGroups(){
+        return battleGroupsRepository.findByStatus(BattleStatus.RECRUITING);
+    }
+    public List<BattleGroups> findAll() {
+        return battleGroupsRepository.findAll();
+    }
+
+    public BattleGroups findById(Long groupId) {
+        return battleGroupsRepository.findById(groupId).orElseThrow(()->new IllegalArgumentException("해당 그룹을 찾을 수 없습니다."));
     }
 }
