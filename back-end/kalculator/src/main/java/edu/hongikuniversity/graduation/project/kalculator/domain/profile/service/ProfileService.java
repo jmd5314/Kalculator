d1970fb6f3ecff221f675fbe8b555a01f625b1c7
jmd5314 <jmd5314@gmail.com>
2024-11-07 22:04:21 +0900

    refactor: #7 프로필 user 조회 리팩토링
    - 매번 DB에서 user를 찾지 않고 authentication에 담긴 user 정보를 쓰도록 변경

--- back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/profile/service/ProfileService.java
+++ back-end/kalculator/src/main/java/edu/hongikuniversity/graduation/project/kalculator/domain/profile/service/ProfileService.java
@@ -1,6 +1,5 @@
 package edu.hongikuniversity.graduation.project.kalculator.domain.profile.service;
 
-import edu.hongikuniversity.graduation.project.kalculator.domain.profile.controller.dto.request.ProfileCreateRequest;
 import edu.hongikuniversity.graduation.project.kalculator.domain.profile.controller.dto.request.ProfileRequest;
 import edu.hongikuniversity.graduation.project.kalculator.domain.profile.controller.dto.request.ProfileUpdateWeightRequest;
 import edu.hongikuniversity.graduation.project.kalculator.domain.profile.controller.dto.response.ProfileCurrentWeightResponse;
@@ -10,24 +9,22 @@ import edu.hongikuniversity.graduation.project.kalculator.domain.profile.entity.
 import edu.hongikuniversity.graduation.project.kalculator.domain.profile.exception.ProfileNotFoundException;
 import edu.hongikuniversity.graduation.project.kalculator.domain.profile.repository.ProfileRepository;
 import edu.hongikuniversity.graduation.project.kalculator.domain.user.entity.User;
-import edu.hongikuniversity.graduation.project.kalculator.domain.user.repository.UserRepository;
-import edu.hongikuniversity.graduation.project.kalculator.global.auth.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static edu.hongikuniversity.graduation.project.kalculator.global.auth.util.SecurityUtil.getCurrentUser;
 
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class ProfilesService {
+public class ProfileService {
     private final ProfileRepository profileRepository;
-    private final UserRepository userRepository;
 
     //프로필 생성
     @Transactional
-    public ProfileIdResponse save(ProfileRequest request) {
+    public ProfileIdResponse create(ProfileRequest request) {
         User user = getCurrentUser();
 
         Profile profile = Profile.builder()
@@ -79,10 +76,6 @@ public class ProfilesService {
         return ProfileCurrentWeightResponse.from(profile);
     }
 
-    private User getCurrentUser(){
-        return userRepository.findByUsername(SecurityUtil.getCurrentUsername())
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다"));
-    }
 
     private Profile findByUser(){
         return profileRepository.findByUser(getCurrentUser())
